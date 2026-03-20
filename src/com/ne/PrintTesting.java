07b3a87a271ebe0ca2cb289694b86e21494675d5
codespell <developer@codespell.ai>
2026-03-20 05:31:58 +0000

    feat: implement changes

---
+++ src/com/ne/PrintTesting.java
@@ -0,0 +1,15 @@
+package com.ne;
+
+public class PrintTesting {
+	
+	public static void main(String[] args) {
+		String text = "API_AUTOMATION_TESTING";
+		System.out.println(text);
+		
+		String[] words = text.split("_");
+		int wordCount = words.length;
+		System.out.println("Word count: " + wordCount);
+	}
+
+}
+
