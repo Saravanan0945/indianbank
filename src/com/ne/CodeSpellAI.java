4a39bac768d6620d8639e47ff0f4a1fc88bbde35
codespell <developer@softspell.ai>
2026-08-10 05:49:40 +0000

    feat: Java

--- src/com/ne/CodeSpellAI.java
+++ src/com/ne/CodeSpellAI.java
@@ -7,6 +7,13 @@ public class CodeSpellAI {
 		String reversed = new StringBuilder(text).reverse().toString();
 		
 		System.out.println(text + " Vice Versa: " + reversed);
+		
+		// Count words in the full phrase
+		String fullPhrase = text + " Vice Versa";
+		String[] words = fullPhrase.split("\\s+");
+		int wordCount = words.length;
+		
+		System.out.println("Word Count: " + wordCount);
 	}
 
 }
