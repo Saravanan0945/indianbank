c56af61ef8f5d31d881eb49f7e35ff3eb94c04e4
codespell <developer@softspell.ai>
2026-08-10 05:48:35 +0000

    feat: Create new Java file to print 'codeSpellAI Vice Versa' and its reverse

---
+++ src/com/ne/CodeSpellAI.java
@@ -0,0 +1,13 @@
+package com.ne;
+
+public class CodeSpellAI {
+	
+	public static void main(String[] args) {
+		String text = "codeSpellAI";
+		String reversed = new StringBuilder(text).reverse().toString();
+		
+		System.out.println(text + " Vice Versa: " + reversed);
+	}
+
+}
+
